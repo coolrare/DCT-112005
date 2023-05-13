8fa82badc2c483939f9d11098b45c218d6cfb5a3
Mike Huang <wellwind@mail2000.com.tw>
2023-05-13 16:33:02 +0800

    示範使用 HttpClient

---
+++ src/app/article.ts
@@ -0,0 +1,10 @@
+export interface Article {
+  id: number;
+  href: string;
+  title: string;
+  date: string;
+  author: string;
+  category: string;
+  'category-link': string;
+  summary: string;
+}
