de712ad561369c90e8ec57d588bef93080365e3c
Mike Huang <wellwind@mail2000.com.tw>
2023-05-13 15:20:28 +0800

    示範屬性型指令 (class, style)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,5 +11,12 @@ import { CommonModule } from '@angular/common';
 export class HeaderComponent {
   title = 'The Will Will Web Demo';
   url = 'http://blog.miniasp.com/';
-  data = { name: 'Mike'};
+  data = { name: 'Mike' };
+  isHighlight = false;
+  h3Color = 'red';
+
+  toggleHighlight() {
+    this.isHighlight = !this.isHighlight;
+    this.h3Color = this.h3Color === 'red' ? 'black' : 'red';
+  }
 }
