d2c2a4cf3ba389a130a2c65e22d052f8577c60f1
Mike Huang <wellwind@mail2000.com.tw>
2023-05-13 13:14:03 +0800

    示範內嵌繫結 ( interpolation )、屬性繫結 ( Property Binding )

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,8 +6,9 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
+  title = 'The Will Will Web Demo';
+  url = 'http://blog.miniasp.com/';
 }
